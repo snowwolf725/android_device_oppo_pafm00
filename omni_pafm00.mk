26e085cd6ac44924caf8d2c9a10daeed94fac2bb
snowwolf725 <snowwolf725@gmail.com>
2019-02-21 08:21:04 +0000

    Add extract-files script & pre-build kernel

--- omni_pafm00.mk
+++ omni_pafm00.mk
@@ -17,10 +17,26 @@
 # Release name
 PRODUCT_RELEASE_NAME := pafm00
 
-$(call inherit-product, build/target/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# must be before including omni part
+TARGET_BOOTANIMATION_SIZE := 1080p
+
+#$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/oppo/pafm00/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_pafm00
@@ -31,7 +47,4 @@ PRODUCT_MANUFACTURER := OPPO
 
 TARGET_VENDOR_PRODUCT_NAME := pafm00
 TARGET_VENDOR_DEVICE_NAME := pafm00
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=pafm00 \
-    BUILD_PRODUCT=pafm00 \
-    PRODUCT_NAME=pafm00
+ALLOW_MISSING_DEPENDENCIES := true
